d703f85bd91228e7989238b9d38cb74b17c4c79a
sheffien <sheffien@users.noreply.github.com>
2015-06-03 07:51:02 +0200

    update bashsetup

--- bashsetup.sh
+++ bashsetup.sh
@@ -1,7 +1,7 @@
 # Source this in .bashrc
 #cd "${BASH_SOURCE%/*}" || exit
 # Bash source makes these relative to this script.
-source ${BASH_SOURCE%/*}/addRepoBin.sh
+source ${BASH_SOURCE%/*}/repo_paths.sh
 source ${BASH_SOURCE%/*}/alias_git.sh
 source ${BASH_SOURCE%/*}/alias_docker.sh
 source ${BASH_SOURCE%/*}/alias_slurm.sh
