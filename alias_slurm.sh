29fd29c6e0b27c904e8f324d7cae5d61450570e3
sheffien <sheffien@users.noreply.github.com>
2015-06-14 00:18:12 +0200

    differentiate running from pending in slurm alias

--- alias_slurm.sh
+++ alias_slurm.sh
@@ -2,7 +2,7 @@
 # To use these, just source this file in your .bashrc
 
 
-alias scount="squeue | awk '{print \$4}' | sort | uniq -c"
+alias scount="squeue | awk '{print \$4 \"(\" \$5 \")\"}' | sort | uniq -c"
 alias myq='squeue -o " %.42j %.6i %.4P %.2t %.8M %.5m %z %c %.4R %n" -u `whoami` -S i; scount'
 
 
