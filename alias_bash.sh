3266d0536cb83f42ad95ba1a6728324d1431c7fe
sheffien <sheffien@users.noreply.github.com>
2015-07-07 10:59:48 +0200

    add disk use functions

--- alias_bash.sh
+++ alias_bash.sh
@@ -23,6 +23,34 @@ find . -maxdepth 1 -mindepth 1 -type d | while read dir; do   printf "%-25.25s :
 # Tar up a folder. Creates an archive of a folder
 tu ()
 { 
-tar -cvzpf $1.tgz $1
+tar -czpf $1.tgz $1
 }
 PYTHONPATH=$PYTHONPATH:~/mypython
+
+diskrep ()
+{
+  diskUse=$(du -s "$1" | cut -f1)
+  #regex="(.*)"
+  #[[ $diskUse =~ $regex ]]
+#  gigUse="${BASH_REMATCH[1]}"
+  
+  # file count
+  fileCount=$(find "$1" -type f | wc -l)
+  # echo $1:  $diskUse : $fileCount
+  
+  if [ $diskUse -gt 1000000 ] || [ $fileCount -gt 50 ]
+    then
+    gigs=$(($diskUse/1000000))
+    echo "$1" ${gigs}G : ${fileCount}
+  fi
+  
+
+}
+
+cf() {
+ find . -maxdepth 1 -mindepth 1 -type d | while read dir; do   printf "%-25.25s : " "$dir";   find "$dir" -type f | wc -l; done
+}
+
+cfdu() {
+  find . -maxdepth 1 -mindepth 1 -type d | while read dir; do diskrep "$dir"; done;
+}
