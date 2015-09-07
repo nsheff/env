cbebc014113ef35e4d2f5afd8dfffa8d3b8caf90
NS <sheffien@users.noreply.github.com>
2015-09-07 14:37:22 +0200

    Update bash_vars_template.sh

--- bash_vars_template.sh
+++ bash_vars_template.sh
@@ -3,17 +3,16 @@
 # These are all pointers to folders that should *include trailing slashes*
 
 # Pointer to the 'raw data' filesystem
-export RAW_DATA="/fhgfs/groups/lab_bsf/samples/"
+export RAWDATA="/fhgfs/groups/lab_bsf/samples/"
 
 # Pointer to the 'processed data' filesystem
-export DATA_BASE="/fhgfs/groups/lab_bock/shared/projects/"
+export PROCESSED="/fhgfs/groups/lab_bock/shared/projects/"
+
+# Pointer to web exported filesystem
+export WEB="/data/groups/lab_bock/public_html/nsheffield/"
 
 # Pointer to the common shared resources directory
-export RESOURCE_DIR="/data/groups/lab_bock/shared/resources/"
+export RESOURCES="/data/groups/lab_bock/shared/resources/"
 
 # Pointer to the collection of git repos
-export CODE_BASE="~/repo/"
-
-# Legacy variable: just points to RGenomeUtils repo
-export SHARE_DIR="~/rpack/RGenomeUtils/"
-# -----------------------------------------------------------------------------
+export CODEBASE="~/repo/"
