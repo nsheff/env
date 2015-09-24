35dc11ace3f6c944f66297519d5eaa97d80ea449
NS <sheffien@users.noreply.github.com>
2015-09-24 15:56:07 +0200

    add git push all alias

--- alias_git.sh
+++ alias_git.sh
@@ -10,10 +10,12 @@ alias gst="git status"
 #alias gau="!git add $(git ls-files -o --exclude-standard)"
 alias gcm="git commit -m"
 alias gpom="git push origin master"
+alias gpa="git push --all"
 alias gpo="git push origin"
 alias glop="git log --pretty=format:'%C(yellow)%h|%Cred%ad|%Cblue%an|%Cgreen%d %Creset%s' --date=short | column -ts'|' | less -r"
 
 
+# git clone alias
 gclo() 
 {
 	git clone git@github.com:$1
