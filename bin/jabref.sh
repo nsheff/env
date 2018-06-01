964a3dbe3635eb7ddc9c5079e8b3ac45b3505fae
nsheff <nsheff@users.noreply.github.com>
2018-06-01 12:40:52 -0400

    Fix #1

--- bin/jabref.sh
+++ bin/jabref.sh
@@ -1,2 +1,2 @@
-#java -jar ~/apps/JabRef-4.0.jar
-java -jar ~/apps/JabRef-4.1-dev--snapshot--2017-12-22--master--d9644d018.jar
+java -jar ~/apps/JabRef-4.2.jar
+#java -jar ~/apps/JabRef-4.1-dev--snapshot--2017-12-22--master--d9644d018.jar
