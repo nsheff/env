05c4dcd7618a7c914657f93df1db7e0e32719bd3
nsheff <nsheff@users.noreply.github.com>
2019-03-13 10:50:01 -0400

    update slurm funcs

--- alias_slurm.sh
+++ alias_slurm.sh
@@ -2,19 +2,41 @@
 # To use these, just source this file in your .bashrc
 
 
+# Count jobs by username
 alias scount="squeue | awk '{print \$4 \"(\" \$5 \")\"}' | sort | uniq -c  | tr \"\n\" \"\t\" | sed 's/\([0-9][0-9]*\) /\1:/g' | sed 's/ //g' | fmt"
+
+# Look at just current user's queue
 alias myq='squeue -o " %.42j %.8i %.4P %.2t %.10M %.7m %z %c %.8R %.8n %.8a" -u `whoami` -S i; scount'
 
-
+# Given a particular user, look at just this user's queue.
 subq() {
 	squeue -o " %.42j %.8i %.4P %.2t %.10M %.7m %z %c %.8R %.8n %.8a" -u $1 -S i;
 }
 
+# Look at the queues for all users in my primary group
 ourq() {
 	primary_group=`groups | cut -f1 -d' '`
 	for x in `getent group $primary_group | cut -f4 -d':' | sed 's/,/ /g' -`; do
 	echo "$x"; subq $x;
 	done
 	scount
+}
+
+# Show historical jobs in recent history for a given user
+subhist() {
+	sacct --starttime `date +%Y-%m-%d -d "-3 month"` --units=G -u $1 -o "User,JobID,Jobname,partition,state,start,elapsed,MaxRss%6,ncpus%1,Account%6"
+}
+
+# Show my recent jobs
+myhist() {
+	subhist `whoami`
+}
+
+# Show personal recent jobs for all members of my primary group
+ourhist() {
+	primary_group=`groups | cut -f1 -d' '`
+	for x in `getent group $primary_group | cut -f4 -d':' | sed 's/,/ /g' -`; do
+	echo "$x"; subhist $x;
+	done
 }
 
