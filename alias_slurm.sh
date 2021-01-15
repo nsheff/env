cf41219606c9da6d5be4c09f3539c4439cc11e5e
nsheff <nathan@code.databio.org>
2021-01-15 14:17:16 -0500

    tweak scount width

--- alias_slurm.sh
+++ alias_slurm.sh
@@ -3,7 +3,7 @@
 
 
 # Count jobs by username
-alias scount="squeue | awk '{print \$4 \"(\" \$5 \")\"}' | sort | uniq -c  | sort -n -k1,1 | tr \"\n\" \"\t\" | sed 's/\([0-9][0-9]*\) /\1:/g' | sed 's/ //g' | fmt"
+alias scount="squeue | awk '{print \$4 \"(\" \$5 \")\"}' | sort | uniq -c  | sort -n -k1,1 | tr \"\n\" \"\t\" | sed 's/\([0-9][0-9]*\) /\1:/g' | sed 's/ //g' | fmt -w 140"
 
 # Look at just current user's queue
 alias myq='squeue -o " %.42j %.8i %.4P %.2t %.10M %.7m %z %c %.8R %.8n %.8a" -u `whoami` -S i; scount'
