248182da7961c99b5c721e33474f782907b531aa
nsheff <nsheff@users.noreply.github.com>
2023-05-25 11:00:31 -0400

    update jabref

--- bin/jabref.sh
+++ bin/jabref.sh
@@ -1,2 +1,4 @@
-java -jar ~/apps/JabRef-4.2.jar
+#java -jar ~/apps/JabRef-4.2.jar
+#java -jar ~/apps/JabRef-5.7-portable_linux/JabRef/bin/JabRef
+~/apps/JabRef-5.9-portable_linux/JabRef/bin/JabRef
 #java -jar ~/apps/JabRef-4.1-dev--snapshot--2017-12-22--master--d9644d018.jar
